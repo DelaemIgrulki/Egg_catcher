5e2eded42c3b73e64ad15aef45d376c59af40221
EB-coder <73636880+EB-coder@users.noreply.github.com>
2020-12-11 22:13:58 +0600

    Update egg-catcher-game.py

--- egg-catcher-game.py
+++ egg-catcher-game.py
@@ -1,1 +1,22 @@
+import random
+from pygame import mixer
+import pygame
 
+#initialize game
+pygame.init()
+
+#initial setup
+screen = pygame.display.set_mode((800, 600))
+background = pygame.image.load('bcimage.jpg')
+pygame.display.set_caption('Egg Catcher')
+
+#player
+playerImg = pygame.image.load('basket.png')
+playerx = 100
+playery = 380
+playery_change = 0
+playerx_change = 0
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
