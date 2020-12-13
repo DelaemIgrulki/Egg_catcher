a2d6f032d576af1ec08ccde024ca40a968ad5c08
EB-coder <73636880+EB-coder@users.noreply.github.com>
2020-12-14 00:09:17 +0600

    Update egg-catcher-game.py

--- egg-catcher-game.py
+++ egg-catcher-game.py
@@ -65,4 +65,33 @@ def showscore(x, y):
     score_value = font.render('Score : ' + str(score), True, (255, 255, 255))
     screen.blit(score_value, (x, y))
 
+# Game over text
+game_font = pygame.font.Font('freesansbold.ttf', 40)
+reason_font = pygame.font.Font('freesansbold.ttf', 20)
+
+
+# function to display game over text
+def game_over_text():
+    text = game_font.render('GAME OVER', True, (255, 255, 255))
+    screen.blit(text, (300, 230))
+    text1 = reason_font.render('Do not catch black eggs', True, (255, 0, 0))
+    screen.blit(text1, (300, 280))
+
+
+# victory_font
+victory_font = pygame.font.Font('freesansbold.ttf', 45)
+
+
+def game_over2():
+    score_value = victory_font.render('GAME OVER', True, (255, 255, 255))
+    screen.blit(score_value, (300, 230))
+    text1 = reason_font.render('Time limit exceeded', True, (255, 0, 0))
+    screen.blit(text1, (350, 280))
+
+
+def victory():
+    score_value = victory_font.render('YOU WON', True, (255, 255, 255))
+    screen.blit(score_value, (300, 230))
+
+
     
